bb588536cddb9005682ba75ef71039fad027d434
Yuxing Huang <gzfelix@gmail.com>
2009-11-01 19:48:59 -0400

    * add sample tests

---
+++ Tests/LocalTests.h
@@ -0,0 +1,26 @@
+//
+//  LocalTests.h
+//  WIRemoting
+//
+//  Created by Felix Huang on 09-11-01.
+//  Copyright 2009 Webinit Consulting. All rights reserved.
+//
+//  See Also: http://developer.apple.com/iphone/library/documentation/Xcode/Conceptual/iphone_development/135-Unit_Testing_Applications/unit_testing_applications.html
+
+//  Application unit tests contain unit test code that must be injected into an application to run correctly.
+//  Define USE_APPLICATION_UNIT_TEST to 0 if the unit test code is designed to be linked into an independent test executable.
+
+#define USE_APPLICATION_UNIT_TEST 1
+
+#import <SenTestingKit/SenTestingKit.h>
+#import <UIKit/UIKit.h>
+//#import "application_headers" as required
+
+
+@interface LocalTests : SenTestCase {
+
+}
+
+- (void) testMath;              // simple standalone test
+
+@end
---
+++ Tests/LocalTests.m
@@ -0,0 +1,22 @@
+//
+//  LocalTests.m
+//  WIRemoting
+//
+//  Created by Felix Huang on 09-11-01.
+//  Copyright 2009 Webinit Consulting. All rights reserved.
+//
+
+#import "LocalTests.h"
+
+
+@implementation LocalTests
+
+
+- (void) testMath {
+    
+    STAssertTrue((1+1)==2, @"Compiler isn't feeling well today :-(" );
+    
+}
+
+
+@end
