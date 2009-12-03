8fe0ef12333d057f839c5457add7c502ff56d6f3
Yuxing Huang <gzfelix@gmail.com>
2009-12-02 21:46:14 -0400

    * wiremoting last change

---
+++ main.m
@@ -0,0 +1,15 @@
+/*
+ *  main.c
+ *  WIRemoting
+ *
+ *  Created by Felix Huang on 09-12-02.
+ *  Copyright 2009 Webinit Consulting. All rights reserved.
+ *
+ */
+
+#import "WIRemoting.h"
+#import "MockupProtocol.h"
+
+int main() {
+  MockupProtocol* protocol = [[MockupProtocol alloc] init];
+}
